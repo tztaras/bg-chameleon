9800bf5db80311f7facf387446694c8e0cd7ab77
tarasend <tz.taras@gmail.com>
2024-04-14 18:46:50 +0300

    #EE204D

--- app-light.js
+++ app-light.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log(window.location.hash);
+    //'#5'
+
+    try {
+
+        if (window.location.hash) {
+            const hash = +window.location.hash.substring(1);
+            console.log(hash);
+
+            if (!isNaN(hash)) {
+                const element = document.querySelector(`ul li:nth-child(${hash})`);
+                console.log(element);
+                if (element) element.style.color = '#EE204D';
+                element.style.textDecoration = 'underline';
+                // element.style.backgroundColor = '#141010';
+            }
+        }
+    }
+        catch (err) {
+            alert('HASH NOT FOUND!');
+        }
 });
