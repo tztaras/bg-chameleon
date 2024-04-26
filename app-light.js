f42fa7d887f2a1b146a68f775009447c16df5e1e
tarasend <tz.taras@gmail.com>
2024-04-26 14:26:51 +0300

    flickSwitch

--- app-light.js
+++ app-light.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(window.location.hash);
     //'#5'
 
+
+
+
     try {
 
         if (window.location.hash) {
@@ -21,3 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('HASH NOT FOUND!');
         }
 });
+
+function flickSwitch(arr) {
+    arr= ['bicycle', 'jarmony', 'flick', 'sheep', 'flick']
+  let flick = true;
+  let res = arr.map(item=> item =='flick' ? flick = !flick : flick);
+  console.log(res)
+    return res
+}
+flickSwitch();
